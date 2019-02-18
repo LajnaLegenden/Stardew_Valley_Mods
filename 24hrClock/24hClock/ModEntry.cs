b09b43a56ec995fda2494342cd2f9c7223f42138
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-02-18 01:47:47 -0500

    update for SMAPI 3.0

--- 24hrClock/24hClock/ModEntry.cs
+++ 24hrClock/24hClock/ModEntry.cs
@@ -16,16 +16,16 @@ namespace Lajna.Mods.MilitaryTime
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            TimeEvents.AfterDayStarted += this.TimeEvents_AfterDayStarted;
+            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
         }
 
         /*********
         ** Private methods
         *********/
-        /// <summary>The method invoked after a new day starts.</summary>
+        /// <summary>Raised after the game begins a new day (including when the player loads a save).</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void TimeEvents_AfterDayStarted(object sender, EventArgs e)
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
         {
             if (!Game1.onScreenMenus.OfType<Clock>().Any())
             {
